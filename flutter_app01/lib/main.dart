a5674d811de00b31e4101220b2f4aabb8e56fa85
Akko <2322264747@qq.com>
2020-05-15 01:08:55 +0800

    Update main.dart

--- flutter_app01/lib/main.dart
+++ flutter_app01/lib/main.dart
@@ -1,24 +1,37 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
 //自定义组件
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Too late')),
+        body: HomeContent(),
+      ),
+      theme: ThemeData(primarySwatch: Colors.pink),
+    );
+  }
+}
+
+//
+class HomeContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Center(
-    child: Text(
+        child: Text(
       'Hello AkkoYan',
       textDirection: TextDirection.ltr,
-
       style: TextStyle(
         fontSize: 40.0,
-        color:Colors.white,
-    ),
-    )
-  );
+        color: Colors.black,
+      ),
+    ));
   }
 }
