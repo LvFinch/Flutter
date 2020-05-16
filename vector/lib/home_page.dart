9ccb94be179218246bf7e3b6d1fd53f541f1e319
Akko <2322264747@qq.com>
2020-05-16 21:40:20 +0800

    Create home_page.dart

---
+++ vector/lib/home_page.dart
@@ -0,0 +1,19 @@
+/*
+ * -> Author : Akko
+ * -> Date : 2020-05-16 11:09:06
+ * -> LastEditTime : 2020-05-16 11:20:28
+ * -> LastEditors : Akko
+ * -> Description : Vector HomePage Code
+ * -> FilePath : \vector\lib\home_page.dart
+ * -> Copyright  © 2020 Akko All rights reserved.
+ */
+import 'package:flutter/widgets.dart';
+
+//MyApp => MaterialApp => Scaffold => HomePage
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Text('别看了,还没写!');
+  }
+}
