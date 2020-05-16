3ea2d5c5abf5a2955a73e530706b6b89e8a6e458
Akko <2322264747@qq.com>
2020-05-16 21:40:14 +0800

    Create bottom_navigation_bar.dart

---
+++ vector/lib/bottom_navigation_bar.dart
@@ -0,0 +1,20 @@
+/*
+ * -> Author : Akko
+ * -> Date : 2020-05-16 15:29:04
+ * -> LastEditTime : 2020-05-16 15:32:43
+ * -> LastEditors : Akko
+ * -> Description : Vector BottomNavigationBar Code
+ * -> FilePath : \vector\lib\bottom_navigation_bar.dart
+ * -> Copyright  © 2020 Akko All rights reserved.
+ */
+// import 'package:flutter/material.dart';
+
+// class BottomBar extends StatelessWidget{
+//   @override
+//   // TODO: implement backgroundColor
+//   Widget build(BuildContext context) {
+//     // TODO: implement build
+//     return 
+//   }
+  
+// }
