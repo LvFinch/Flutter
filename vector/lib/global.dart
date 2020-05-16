5df38f4e5ef714571a1413f7c887767037dfa666
Akko <2322264747@qq.com>
2020-05-16 21:40:18 +0800

    Create global.dart

---
+++ vector/lib/global.dart
@@ -0,0 +1,17 @@
+/*
+ * -> Author : Akko
+ * -> Date : 2020-05-16 15:52:01
+ * -> LastEditTime : 2020-05-16 15:54:53
+ * -> LastEditors : Akko
+ * -> Description : Vector Global Code
+ * -> FilePath : \vector\lib\global.dart
+ * -> Copyright  © 2020 Akko All rights reserved.
+ */
+import 'package:flutter/material.dart';
+
+const _vectorColor = <MaterialColor>[
+  Colors.red,
+  Colors.blueGrey,
+  Colors.green,
+  Colors.yellow,
+];
