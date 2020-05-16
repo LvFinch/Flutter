fabf4dad7d0fff441d07ca689d64249e6d9c82b9
Akko <2322264747@qq.com>
2020-05-16 21:40:23 +0800

    Update main.dart

--- vector/lib/main.dart
+++ vector/lib/main.dart
@@ -1,16 +1,20 @@
 /*
  * -> Author : Akko
  * -> Date : 2020-05-15 18:05:18
- * -> LastEditTime : 2020-05-16 01:31:09
+ * -> LastEditTime : 2020-05-16 21:18:39
  * -> LastEditors : Akko
  * -> Description : Vector main code
  * -> FilePath : \vector\lib\main.dart
  * -> Copyright  © 2020 Akko All rights reserved.
  */
 import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:common_utils/common_utils.dart';
+
+import './home_page.dart';
+import './app_bar.dart';
+import './global.dart';
 
 void main() {
   runApp(MyApp());
@@ -31,66 +35,35 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         // backgroundColor: Colors.transparent,
         appBar: AppBar(
-          backgroundColor: Colors.blueGrey,
-          // elevation: 0,
-          // leading: VectorAppBarLeading(),
+          backgroundColor: Colors.black38,
+          elevation: 0,
           title: VectorAppBarTitle(),
+          toolbarOpacity: 1,
         ),
         body: HomePage(),
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(30), topLeft: Radius.circular(30)),
+            boxShadow: [
+              BoxShadow(color: Colors.black38, spreadRadius: 0, blurRadius: 10),
+            ],
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30), topRight: Radius.circular(30)),
+            child: BottomNavigationBar(
+              items: <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.home), title: Text('主页')),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.settings), title: Text('设置'))
+              ],
+            ),
+          ),
+        ),
       ),
-      theme: ThemeData(primarySwatch: Colors.pink),
+      theme: ThemeData(primarySwatch: Colors.blue),
     );
-  }
-}
-
-//MyApp => MaterialApp => Scaffold => AppBar => VectorAppBarTitle
-class VectorAppBarTitle extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: AppBar Title
-    return VectorAppBarTitleHint();
-  }
-}
-
-//MyApp => MaterialApp => Scaffold => AppBar => VectorAppBarTitle => VectorAppBarTitleHint
-class VectorAppBarTitleHint extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    DateTime NowTime = DateTime.now();
-    // DateFormat _NowTime = DateFormat.HOUR_MINUTE;
-    // _NowTime = DateFormat.HOUR_MINUTE;
-    // if (_NowTime ) {
-
-    // } else {
-    // }
-
-    return Container(
-      child: Text("$NowTime good night"),
-    );
-  }
-}
-
-// //MyApp => MaterialApp => Scaffold => AppBar => VectorAppBarLeading
-// class VectorAppBarLeading extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     //TODO: Delay! AppBar Leading
-//     return Container(
-//       child: Image.asset(
-//         'images/vector.png',
-//         height: 5,
-//         width: 5,
-//       ),
-//     );
-//   }
-// }
-
-//MyApp => MaterialApp => Scaffold => HomePage
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Text('别看了,还没写');
   }
 }
