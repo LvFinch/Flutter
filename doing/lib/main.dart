cc061e63b178bb2f48bc412d0363fa0b09643be4
Akko <2322264747@qq.com>
2020-05-22 00:48:13 +0800

    Update main.dart

--- doing/lib/main.dart
+++ doing/lib/main.dart
@@ -1,19 +1,27 @@
 /*
  * -> Author : Akko
  * -> Date : 2020-05-20 17:06:25
- * -> LastEditTime : 2020-05-21 23:13:14
+ * -> LastEditTime : 2020-05-22 00:03:42
  * -> LastEditors : Akko
  * -> Description : Main
  * -> FilePath : \doing\lib\main.dart
  * -> Copyright  © 2020 Akko All rights reserved.
  */
 
+import 'dart:io';
+
 import 'package:doing/pages/home_page.dart';
 import 'package:doing/routes/test.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
+    if (Platform.isAndroid) {
+    // 以下两行 设置android状态栏为透明的沉浸。写在组件渲染之后，是为了在渲染后进行set赋值，覆盖状态栏，写在渲染之前MaterialApp组件会覆盖掉这个值。
+	SystemUiOverlayStyle systemUiOverlayStyle = SystemUiOverlayStyle(statusBarColor: Colors.transparent);		
+	SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+}
 }
 
 class MyApp extends StatelessWidget {
