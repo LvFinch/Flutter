b4f43fe69f3770a4c9b96790f0f1fdb92b9f5519
Akko <2322264747@qq.com>
2020-05-21 23:37:42 +0800

    Update home_page.dart

--- doing/lib/pages/home_page.dart
+++ doing/lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 /*
  * -> Author : Akko
  * -> Date : 2020-05-20 23:50:55
- * -> LastEditTime : 2020-05-21 16:33:52
+ * -> LastEditTime : 2020-05-21 23:29:38
  * -> LastEditors : Akko
  * -> Description : Home Page
  * -> FilePath : \doing\lib\pages\home_page.dart
@@ -23,12 +23,62 @@ class _DoingHomePageState extends State<DoingHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        backgroundColor: Color.fromARGB(255, 135, 232, 222),
-      ),
-      body: Center(
-        child: Text("test"),
+      // appBar: AppBar(
+      //   title: Text(widget.title),
+      //   backgroundColor: Color.fromARGB(255, 135, 232, 222),
+      //   elevation: 1.0,
+      // ),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          ///First sliver is the App Bar
+          SliverAppBar(
+            leading: Icon(
+              AntDesign.menu_unfold,
+              color: Color.fromARGB(255, 115, 166, 212),
+            ),
+            elevation: 1.0,
+            backgroundColor: Color.fromARGB(255, 135, 232, 222),
+            floating: false, //true立刻收起,false跟随收起
+            pinned: true,
+            expandedHeight: 250.0,
+            flexibleSpace: FlexibleSpaceBar(
+              centerTitle: true,
+              title: Text(
+                "Just Do It",
+                style: TextStyle(
+                  color: Color.fromARGB(255, 115, 166, 212),
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              background: Container(
+                color: Color.fromARGB(255, 105, 192, 255),
+                child: Center(
+                  child: Text("test"),
+                ),
+              ),
+            ),
+          ),
+          SliverFixedExtentList(
+            itemExtent: 100.0,
+            delegate: SliverChildListDelegate(
+              [
+                Icon(Icons.ac_unit),
+                Icon(Icons.ac_unit),
+                Icon(Icons.ac_unit),
+                Icon(Icons.ac_unit),
+                Icon(Icons.ac_unit),
+                Icon(Icons.ac_unit),
+                Icon(Icons.ac_unit),
+                Icon(Icons.ac_unit),
+                Icon(Icons.ac_unit),
+                Icon(Icons.ac_unit),
+                Icon(Icons.ac_unit),
+                Icon(Icons.ac_unit),
+              ],
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
