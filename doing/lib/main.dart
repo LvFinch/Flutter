ee0f2a3848b45e3b6948ba16657bece4517b6f7c
Akko <2322264747@qq.com>
2020-05-22 22:17:11 +0800

    Update main.dart

--- doing/lib/main.dart
+++ doing/lib/main.dart
@@ -1,7 +1,7 @@
 /*
  * -> Author : Akko
  * -> Date : 2020-05-20 17:06:25
- * -> LastEditTime : 2020-05-22 00:03:42
+ * -> LastEditTime : 2020-05-22 19:23:32
  * -> LastEditors : Akko
  * -> Description : Main
  * -> FilePath : \doing\lib\main.dart
@@ -11,17 +11,19 @@
 import 'dart:io';
 
 import 'package:doing/pages/home_page.dart';
+import 'package:doing/routes/help.dart';
 import 'package:doing/routes/test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
-    if (Platform.isAndroid) {
+  if (Platform.isAndroid) {
     // 以下两行 设置android状态栏为透明的沉浸。写在组件渲染之后，是为了在渲染后进行set赋值，覆盖状态栏，写在渲染之前MaterialApp组件会覆盖掉这个值。
-	SystemUiOverlayStyle systemUiOverlayStyle = SystemUiOverlayStyle(statusBarColor: Colors.transparent);		
-	SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
-}
+    SystemUiOverlayStyle systemUiOverlayStyle =
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+    SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -31,12 +33,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Doing',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       routes: {
         "test_route": (context) => TestRoute(),
-        "home_page":(context)=> DoingHomePage(title: "test route",),
+        "help_route": (context) => HelpRoute(),
+        "home_page": (context) => DoingHomePage(
+              title: "test route",
+            ),
       },
       home: DoingHomePage(
         title: "Doing",
